1177ff1a741d42838a2aedc3ec4c90c63d26bdad
bkdevduniya <bhoodev6232@gmail.com>
2025-03-13 20:08:29 +0530

    Update multivariate.py

--- EDA/multivariate_analysis/multivariate.py
+++ EDA/multivariate_analysis/multivariate.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 
+df=pd.read_csv('cleaned_data.csv')
+
+#drawing the pair plot  between price and age
+sns.pairplot(df)
+plt.show()
+
+# drawing the heatmap between price and age
+sns.heatmap(df[['price','age']].corr(),annot=True)
+plt.show()
